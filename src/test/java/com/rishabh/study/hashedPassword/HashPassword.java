976641fe7541c7b5564e26c2a50d16f6f59aca93
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-09-25 21:43:21 +0530

    using bcrypt hash

--- src/test/java/com/rishabh/study/hashedPassword/HashPassword.java
+++ src/test/java/com/rishabh/study/hashedPassword/HashPassword.java
@@ -1,16 +1,27 @@
 package com.rishabh.study.hashedPassword;
 
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
-public class MD5Hash extends TestCase {
+public class HashPassword extends TestCase {
 
 	public void testConvertToMD5() {
 		String password = "secret";
 		Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
 		String hashedKey = passwordEncoder.encodePassword(password, null);
+		// Not using Salt here.
+		
+		Assert.assertNotNull(hashedKey);
+		System.out.println("Encoded String: "+hashedKey);
+	}
+	
+	public void testConvertToBcrypt() {
+		String password = "secret";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedKey = passwordEncoder.encode(password);
 		// Not using Salt here.
 		
 		Assert.assertNotNull(hashedKey);
